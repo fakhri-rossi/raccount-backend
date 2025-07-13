007a407cc93631be90048a426db2a8e7faf100d1
Fakhri Rossi <rossifakhri@gmail.com>
2025-07-13 11:21:36 +0700

    Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      process.env.DB_URL || 'mongodb://localhost:27017/mydb',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
