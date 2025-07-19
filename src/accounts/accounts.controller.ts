4045abd41dfda31733f9069e5ecf1a91dc16f47b
Fakhri Rossi <rossifakhri@gmail.com>
2025-07-19 17:06:32 +0700

    feat: serch account and find account by id

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,13 +1,43 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { CreateAccountDto } from './dto/create-account.dto';
+import { Account } from './schemas/account.schema';
+import { AccountType } from 'src/common/enums/accountType.enum';
 
 @Controller('accounts')
 export class AccountsController {
   constructor(private accountService: AccountsService) {}
 
   @Post()
-  async create(@Body() createAccountDto: CreateAccountDto) {
+  async create(@Body() createAccountDto: CreateAccountDto): Promise<Account> {
     return this.accountService.create(createAccountDto);
+  }
+
+  @Get()
+  async search(
+    @Query('code') code?: string,
+    @Query('type') type?: AccountType,
+    @Query('parentId') parentId?: string,
+    @Query('level') level?: number,
+    @Query('name') name?: string,
+    @Query('skip') skip?: number,
+    @Query('limit') limit?: number,
+  ): Promise<Account[]> {
+    return this.accountService.find({
+      code,
+      type,
+      parentId,
+      level,
+      skip,
+      limit,
+      name,
+    });
+  }
+
+  @Get('/:id')
+  async findAcccountById(
+    @Param('id') accountId: string,
+  ): Promise<Account | null> {
+    return this.accountService.findById(accountId);
   }
 }
--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -3,10 +3,21 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import { Account } from './schemas/account.schema';
-import { Model } from 'mongoose';
+import { Account, AccountDocument } from './schemas/account.schema';
+import { FilterQuery, Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateAccountDto } from './dto/create-account.dto';
+import { AccountType } from 'src/common/enums/accountType.enum';
+
+export interface FindAccountOptions {
+  code?: string;
+  type?: AccountType;
+  parentId?: string;
+  level?: number;
+  name?: string;
+  skip?: number;
+  limit?: number;
+}
 
 @Injectable()
 export class AccountsService {
@@ -45,5 +56,27 @@ export class AccountsService {
       isActive: true,
     });
     return createdAccount.save();
+  }
+
+  async find(options: FindAccountOptions = {}): Promise<Account[]> {
+    const { code, level, name, parentId, type, skip = 0, limit = 20 } = options;
+
+    const filter: FilterQuery<AccountDocument> = {};
+
+    if (name) filter.name = { $regex: name, $options: 'i' };
+
+    if (code) filter.code = code;
+
+    if (level) filter.level = level;
+
+    if (parentId) filter.parentId = parentId;
+
+    if (type) filter.type = type;
+
+    return await this.accountModel.find(filter).skip(skip).limit(limit);
+  }
+
+  async findById(id: string): Promise<Account | null> {
+    return this.accountModel.findById(id);
   }
 }
