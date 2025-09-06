6bb775aa419bc1a8ffdc802d2b930dcda3ab1a56
Fakhri Rossi <rossifakhri@gmail.com>
2025-09-07 04:52:28 +0700

    feat(category): Add API Property to Search Category DTO

--- src/categories/dto/search-category.dto.ts
+++ src/categories/dto/search-category.dto.ts
@@ -1,25 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class SearchCategoryDto {
+  @ApiProperty({ required: false, example: 'Asset' })
   @IsString()
   @IsOptional()
   name?: string;
 
+  @ApiProperty({ required: false, example: 1 })
   @IsNumber()
   @IsOptional()
   @Type(() => Number)
   codePrefix?: number;
 
+  @ApiProperty({ required: false, example: true })
   @IsBoolean()
   @IsOptional()
   isNormalBalanceDebit?: boolean;
 
+  @ApiProperty({ required: false })
   @IsNumber()
   @IsOptional()
   @Type(() => Number)
   skip?: number;
 
+  @ApiProperty({ required: false })
   @IsNumber()
   @IsOptional()
   @Type(() => Number)
